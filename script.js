1628bd125c952a6e3064531c2e80c71ec45db2f5
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-04-11 17:38:55 -0700

    added media queries for smaller resolution

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// variables
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const container = document.querySelector('.container')
 
+// adding listener for left side
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () =>
+  container.classList.remove('hover-left')
+)
+
+// adding listener for right side
+right.addEventListener('mouseenter', () =>
+  container.classList.add('hover-right')
+)
+right.addEventListener('mouseleave', () =>
+  container.classList.remove('hover-right')
+)
